1a93ac44b5eb151a67ee7885672e0c7fb53a6f53
eqselans <emrhn.axu.soft1@gmail.com>
2025-04-20 17:58:15 +0300

    Postgre şema ve init scripti

---
+++ postgres/init.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS weather_data (
+    city        TEXT,
+    temperature FLOAT,
+    humidity    INT,
+    pressure    INT,
+    condition   TEXT,
+    timestamp   TIMESTAMP
+);
